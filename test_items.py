b4059d2d7dcf6d5c6c37a36efd0656d14d5f1a85
IgorTarasovj <igortarasovj@gmail.com>
2026-01-17 19:38:20 +0300

    add test_items.py

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_button(browser):
+    browser.get(link)
+
+    time.sleep(5)
+
+    buttons = browser.find_elements(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+
+    assert len(buttons) > 0, "Element does not exist"
